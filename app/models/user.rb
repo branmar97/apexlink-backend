be64d21ee216607f21d5d6aad2fa4ea3a733e0a5
branmar97 <brandonmar97@gmail.com>
2021-03-08 18:58:49 -0500

    Added gamertag and platform attributes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable,
