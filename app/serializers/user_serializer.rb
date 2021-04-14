328c0d31b0d0f55b4a4c0002c9109d8717f7476f
branmar97 <brandonmar97@gmail.com>
2021-04-14 11:48:18 -0400

    Added region to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :gamertag, :platform, :created_at
+  attributes :id, :email, :gamertag, :platform, :region, :created_at
 end
