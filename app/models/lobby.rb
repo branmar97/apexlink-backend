4292babe8205ad95dac61c2b4cd4854188a7429b
branmar97 <brandonmar97@gmail.com>
2021-06-01 08:22:24 -0400

    add validations to lobby model

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -2,4 +2,14 @@ class Lobby < ApplicationRecord
   belongs_to :user
   has_many :requests, dependent: :destroy
   has_many :users, through: :requests
+
+  validates :user, presence: true
+  validates :region, presence: true
+  validates :platform, presence: true
+  validates :gamemode, presence: true
+  validates :description, presence: true
+  validates :description, length: { minimum: 50 }
+  validates :mic_required, presence: true
+  validates :skill_level, presence: true
+  validates :gamertag, presence: true
 end
