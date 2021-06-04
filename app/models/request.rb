d9030111c2e27783f288dbaeb5fb4f541be3c47f
branmar97 <brandonmar97@gmail.com>
2021-06-03 21:38:30 -0400

    add validations with custom errors

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,9 @@ class Request < ApplicationRecord
   belongs_to :lobby
   belongs_to :user
 
-  validates :user_id, uniqueness: { scope: :lobby_id }
+  validates :user_id, uniqueness: { scope: :lobby_id, message: "Only one request permitted per lobby" }
+  validates :description, length: { maximum: 100,
+    message: "%{count} characters is the maximum allowed" }
+  validates :description, length: { minimum: 10,
+    message: "Must be at least %{count} characters" }
 end
