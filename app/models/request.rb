aafc7e46d51cb884ea3a40f089a4d4886964e9cb
branmar97 <brandonmar97@gmail.com>
2021-06-05 08:22:44 -0400

    add validations for lobby host

--- app/models/request.rb
+++ app/models/request.rb
@@ -7,4 +7,14 @@ class Request < ApplicationRecord
     message: "%{count} characters is the maximum allowed" }
   validates :description, length: { minimum: 10,
     message: "Must be at least %{count} characters" }
+  validate :not_lobby_admin
+
+
+  private
+
+  def not_lobby_admin
+    if self.user_id == self.lobby.user_id
+      errors.add(:is_admin, "Lobby host cannot make a request")
+    end
+  end
 end
