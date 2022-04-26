3a84d6db1d409afc44dfdd490de23ea892ad313a
branmar97 <brandonmar97@gmail.com>
2022-04-26 14:27:50 -0400

    add profile length validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :bio, length: { maximum: 150,
+  message: "Bio must be no more than %{count} characters" }
+  validates :link, length: { maximum: 200,
+  message: "Link must be no more than %{count} characters" }
 end
