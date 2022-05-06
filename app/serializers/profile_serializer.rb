c712c33f0fcd035903a48782943aeddf7f80bae4
branmar97 <brandonmar97@gmail.com>
2022-05-06 08:28:49 -0400

    add avatar url to profile serializer

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,4 +1,4 @@
 class ProfileSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :bio, :link, :slug, :updated_at, :user
+  attributes :bio, :link, :avatar_url, :slug, :updated_at, :user
 end
